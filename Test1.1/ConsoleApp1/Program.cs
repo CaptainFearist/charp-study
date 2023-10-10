464bcd4dd419fbc14c4d4802fb39d6aa44125df4
Igor <114694615+CaptainFearist@users.noreply.github.com>
2023-10-10 12:14:57 +0300

    Add files via upload

---
+++ Test1.1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program1
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число: ");
+        int number;
+        bool Proverka = Int32.TryParse(Console.ReadLine(), out number);
+
+        if (Proverka)
+        {
+            if (number % 3 == 0)
+            {
+                Console.WriteLine("Число делится на 3.");
+            }
+            else
+            {
+                Console.WriteLine("Число не делится на 3.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Неверный ввод");
+        }
+    }
+}
